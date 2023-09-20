08b6dee86e637b36b46af946ee41f36ed0d3cd1c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-19 22:19:16 -0400

    chore: controller and errors

---
+++ src/controller/movies_controller.rs
@@ -0,0 +1,48 @@
+use axum::response::Result;
+use std::sync::{Arc, Mutex};
+
+use crate::model::movies_model::{Movie, MovieForCreate};
+
+#[derive(Clone)]
+pub struct MovieController {
+    movies_store: Arc<Mutex<Vec<Option<Movie>>>>,
+}
+
+impl MovieController {
+    pub async fn new() -> Result<Self> {
+        Ok(Self {
+            movies_store: Arc::default(),
+        })
+    }
+
+    pub async fn create_movie(&self, movie_for_create: MovieForCreate) -> Result<Movie> {
+        let mut store = self.movies_store.lock().unwrap();
+
+        let movie = Movie {
+            id: store.len() as i32 + 1,
+            title: movie_for_create.title,
+            year: movie_for_create.year,
+            length: movie_for_create.length,
+            genre: movie_for_create.genre,
+            studio: movie_for_create.studio,
+            rating: movie_for_create.rating,
+            format: movie_for_create.format,
+            owner: movie_for_create.owner,
+        };
+
+        store.push(Some(movie.clone()));
+
+        Ok(movie)
+    }
+
+    pub async fn list_movies(&self) -> Result<Vec<Movie>> {
+        let store = self.movies_store.lock().unwrap();
+
+        let movies = store
+            .iter()
+            .filter_map(|movie| movie.clone())
+            .collect::<Vec<_>>();
+
+        Ok(movies)
+    }
+}
---
+++ src/error.rs
@@ -0,0 +1,40 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::Serialize;
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Clone, Debug, Serialize)]
+#[serde(tag = "type", content = "data")]
+pub enum Error {}
+
+// region:    --- Error Boilerplate
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        println!("->> {:<12} - {self:?}", "INTO_RES");
+
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> StatusCode {
+        match self {
+            // -- 400 Bad Request.
+            // -- Fallback.
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
