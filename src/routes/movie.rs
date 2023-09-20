4926fe9019a0c6c66234d943455d910fcaff0365
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-19 22:19:28 -0400

    chore: routes

---
+++ src/routes/movie.rs
@@ -0,0 +1,22 @@
+use axum::{extract::State, Json, Router};
+
+use crate::{
+    controller::movies_controller::MovieController,
+    error::Result,
+    model::movies_model::{Movie, MovieForCreate},
+};
+
+pub fn routes() -> Router {
+    todo!()
+}
+
+async fn create_movie(
+    State(mc): State<MovieController>,
+    Json(movie_for_create): Json<MovieForCreate>,
+) -> Result<Json<Movie>> {
+    println!("->> {:<12} - CREATE_MOVIE", "HANDLER");
+
+    todo!("Create a movie");
+
+    // Ok(Json(movie))
+}
