152ef3182475f966fcb4651a9efce1cd94f494d6
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-19 22:19:36 -0400

    chore: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod controller;
+mod error;
+mod model;
+mod routes;
+
+use std::net::SocketAddr;
+
+use axum::{Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("->> LISTENING ON {addr}\n");
+    Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .expect("Failed to initialize the Movies API server");
 }
