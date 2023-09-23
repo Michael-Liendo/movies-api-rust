f670c75270cd530ba7528d07cd0c0e17c5bc3f9d
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-22 22:09:24 -0400

    feat: implement routes

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,16 @@ use axum::{Router, Server};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new();
+    let mc = controller::movies_controller::MovieController::new()
+        .await
+        .unwrap();
+
+    let routes_apis = routes::movie::routes(mc.clone());
+
+    let router = Router::new().nest("/api", routes_apis);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("->> LISTENING ON {addr}\n");
+    println!("->> LISTENING ON http://{addr}\n");
     Server::bind(&addr)
         .serve(router.into_make_service())
         .await
